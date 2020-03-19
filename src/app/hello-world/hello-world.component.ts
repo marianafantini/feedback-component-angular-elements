127d2d237d5602267128103d380515e99b6446b9
Mariana Fantini <mariana.fantini@gmail.com>
2020-03-19 09:20:54 -0300

    HelloWorldComponent is now a custom element and can be added dynamicall

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.scss']
 })
