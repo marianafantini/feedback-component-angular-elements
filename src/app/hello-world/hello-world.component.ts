795dcd522d7cef58f2124296ec934794d7d39622
Mariana Fantini <mariana.fantini@gmail.com>
2020-03-29 22:09:02 -0300

    Inputs and outputs are working - including async ones

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   templateUrl: './hello-world.component.html',
-  styleUrls: ['./hello-world.component.scss']
+  styleUrls: ['./hello-world.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HelloWorldComponent implements OnInit {
+export class HelloWorldComponent implements OnInit, OnChanges{
   @Input() title = '';
+  @Input('to-do-list') toDoList = '';
   @Output() clicked = new EventEmitter();
+
+  toDoList$ = new BehaviorSubject<any>('');
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() { }
 
   onButtonClicked() {
-    this.clicked.emit(true);
+    const completedTasksCount = this.toDoList$.value.filter((task) => task.done).length;
+    this.clicked.emit(completedTasksCount);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['toDoList']) {
+      this.toDoList$.next(JSON.parse(changes['toDoList'].currentValue));
+    }
   }
 
 }
