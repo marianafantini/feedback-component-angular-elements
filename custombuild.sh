8e84f5358fe17262063b32fb10ac7c6d9faeaa39
Mariana Fantini <mariana.fantini@gmail.com>
2020-04-19 23:54:54 -0300

    Improved custom build shell script

--- custombuild.sh
+++ custombuild.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ng build feedback-component-angular-elements --prod --output-hashing=none && cat dist/feedback-component-angular-elements/runtime-es2015.js dist/feedback-component-angular-elements/polyfills-es2015.js dist/feedback-component-angular-elements/main-es2015.js > docs/feedback-component-custom-element.js && cat dist/feedback-component-angular-elements/styles.css > docs/feedback-component-custom-element.css
+PROJECT_NAME=${PWD##*/}
+ng build $PROJECT_NAME --prod --output-hashing=none && cat dist/$PROJECT_NAME/runtime-es2015.js dist/$PROJECT_NAME/polyfills-es2015.js dist/$PROJECT_NAME/main-es2015.js > docs/app-$PROJECT_NAME.js && cat dist/$PROJECT_NAME/styles.css > docs/app-$PROJECT_NAME.css
